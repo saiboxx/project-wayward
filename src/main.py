63a0c8f460529b65def315334335e8cf9ca4c849
Tobias Weber <t.weber@campus.lmu.de>
2019-12-05 16:36:42 +0100

    added random inputs for environment

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 from mlagents.envs.environment import UnityEnvironment
+import numpy as np
 
 from src.agent import Agent
 
@@ -12,11 +13,26 @@ def main():
     with open("config.yml", 'r') as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-    print("Loading environment " + cfg["EXECUTABLE"] + ".")
+    print("Loading environment {}.".format(cfg["EXECUTABLE"]))
     env = load_environment(cfg["EXECUTABLE"])
+    env.reset()
 
     print("Creating Agent.")
     agent = Agent()
+
+    print("Starting training with {} steps.".format(cfg["STEPS"]))
+    acc_reward = 0
+    for steps in range(cfg["STEPS"]):
+        info = env.step(np.random.randn(10, 20))
+        brain_info = info[env.external_brain_names[0]]
+
+        acc_reward += sum(brain_info.rewards) / len(brain_info.rewards)
+
+        if steps % cfg["VERBOSE_STEPS"] == 0:
+            print("Total reward with {0} steps: {1:.5f}".format(steps, acc_reward))
+
+    print("Closing environment.")
+    env.close()
 
 
 def load_environment(env_name: str) -> UnityEnvironment:
