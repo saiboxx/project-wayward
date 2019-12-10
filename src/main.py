f619edfe5459350f12ba580fce7b44ef98bbebe1
Tobias Weber <t.weber@campus.lmu.de>
2019-12-10 09:28:42 +0100

    added episodes mean

--- src/main.py
+++ src/main.py
@@ -27,22 +27,36 @@ def main():
     print("Starting training with {} steps.".format(cfg["STEPS"]))
     acc_reward = 0
     mean_reward = 0
+    reward_cur_episode = []
+    mean_reward_episodes = 0
+    episode = 1
     for steps in range(cfg["STEPS"]):
         action = agent.actor.predict(state, use_target=False)
         info = env.step(action)
         brain_info = info[env.external_brain_names[0]]
         new_state = brain_info.vector_observations
         reward = brain_info.rewards
+        done = brain_info.local_done[0]
         agent.replay_buffer.add(state, action, reward, new_state)
         agent.learn()
 
-        acc_reward += sum(brain_info.rewards) / len(brain_info.rewards)
-        mean_reward += sum(brain_info.rewards) / len(brain_info.rewards)
+        mean_step = sum(brain_info.rewards) / len(brain_info.rewards)
+        acc_reward += mean_step
+        mean_reward += mean_step
+        reward_cur_episode.append(brain_info.rewards[0])
 
         if steps % cfg["VERBOSE_STEPS"] == 0:
             mean_reward = mean_reward / cfg["VERBOSE_STEPS"]
-            print("Mean reward with {0} steps: {1:.5f}".format(steps, mean_reward))
+            print("Ep {0} with {1} steps total : {2:.3f} mean ep. reward; {3:.3f} step reward"\
+                  .format(episode, steps, mean_reward_episodes, mean_reward))
             mean_reward = 0
+
+        if done:
+            mean_reward_episodes += (sum(reward_cur_episode) - mean_reward_episodes) / episode
+            episode += 1
+            info = env.reset()
+            brain_info = info[env.external_brain_names[0]]
+            new_state = brain_info.vector_observations
 
         state = new_state
 
