fix off by one error in step count

src/main_unity.py
@@ -41,7 +41,7 @@ def main():
     start_time_episode = time.time()
     episode = 1
     start_time = time.time()
-    for steps in range(1, cfg["STEPS"]):
+    for steps in range(1, cfg["STEPS"] + 1):
         if steps <= cfg["BUFFER_SIZE"]:
             action = np.random.uniform(-1, 1, size=(len(state), action_space))
         else:

src/run.py
@@ -37,7 +37,7 @@ def main():
     episode = 1
     reward_last_episode = 0
     start_time = time.time()
-    for steps in range(1, cfg["RUN_STEPS"]):
+    for steps in range(1, cfg["RUN_STEPS"] + 1):
         with torch.no_grad():
             action = actor(from_numpy(np.array(state)).float())
         action = action.cpu().numpy()