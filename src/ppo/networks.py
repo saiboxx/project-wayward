d2c6a9f4db38327317f8e108cd2fbdcf62342cb5
Tobias Weber <t.weber@campus.lmu.de>
2019-12-21 15:23:44 +0100

    Added Batch Normalization and Weight Initialization

--- src/ppo/networks.py
+++ src/ppo/networks.py
@@ -7,17 +7,26 @@ class Actor(nn.Module):
                  layer_sizes: list, std: float):
         super().__init__()
         self.fc1 = nn.Linear(observation_space, layer_sizes[0])
+        self.bn1 = nn.LayerNorm(layer_sizes[0])
         self.fc2 = nn.Linear(layer_sizes[0], layer_sizes[1])
+        self.bn2 = nn.LayerNorm(layer_sizes[1])
         self.fc3 = nn.Linear(layer_sizes[1], action_space)
 
         self.leaky_relu = nn.LeakyReLU()
         self.tanh = nn.Tanh()
 
+        self.fc1.weight.data.uniform_(-0.003, 0.003)
+        self.fc1.bias.data.uniform_(-0.003, 0.003)
+        self.fc2.weight.data.uniform_(-0.003, 0.003)
+        self.fc2.bias.data.uniform_(-0.003, 0.003)
+        self.fc3.weight.data.uniform_(-0.003, 0.003)
+        self.fc3.bias.data.uniform_(-0.003, 0.003)
+
         self.log_std = nn.Parameter(tensor([std for _ in range(action_space)]))
 
     def forward(self, state: tensor) -> tensor:
-        h1 = self.leaky_relu(self.fc1(state))
-        h2 = self.leaky_relu(self.fc2(h1))
+        h1 = self.bn1(self.leaky_relu(self.fc1(state)))
+        h2 = self.bn2(self.leaky_relu(self.fc2(h1)))
         mu = self.tanh(self.fc3(h2))
         std = self.log_std.exp().expand_as(mu)
         act_dist = Normal(mu, std)
@@ -29,14 +38,23 @@ class Critic(nn.Module):
         super().__init__()
 
         self.fc1 = nn.Linear(observation_space, layer_sizes[0])
+        self.bn1 = nn.LayerNorm(layer_sizes[0])
         self.fc2 = nn.Linear(layer_sizes[0], layer_sizes[1])
+        self.bn2 = nn.LayerNorm(layer_sizes[1])
         self.fc3 = nn.Linear(layer_sizes[1], 1)
 
         self.leaky_relu = nn.LeakyReLU()
 
+        self.fc1.weight.data.uniform_(-0.003, 0.003)
+        self.fc1.bias.data.uniform_(-0.003, 0.003)
+        self.fc2.weight.data.uniform_(-0.003, 0.003)
+        self.fc2.bias.data.uniform_(-0.003, 0.003)
+        self.fc3.weight.data.uniform_(-0.003, 0.003)
+        self.fc3.bias.data.uniform_(-0.003, 0.003)
+
     def forward(self, state: tensor) -> tensor:
-        h1 = self.leaky_relu(self.fc1(state))
-        h2 = self.leaky_relu(self.fc2(h1))
+        h1 = self.bn1(self.leaky_relu(self.fc1(state)))
+        h2 = self.bn2(self.leaky_relu(self.fc2(h1)))
         out = self.fc3(h2)
         return out
 
