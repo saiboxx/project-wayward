6f55aa17db9dee1370a710a423786ec49b0dafc8
Dervishi <erjona.dervishi@outlook.com>
2020-01-12 14:35:31 +0100

    Implement circular replay buffer

--- src/ddpg/replay_buffer.py
+++ src/ddpg/replay_buffer.py
@@ -3,36 +3,36 @@ import numpy as np
 import yaml
 from operator import itemgetter
 
+
 class ReplayBuffer(object):
 
-    def __init__(self):
+    def __init__(self, state_size, action_size):
         with open("config.yml", 'r') as ymlfile:
             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
         self.max_buffer_size = cfg["BUFFER_SIZE"]
         self.cur_buffer_size = 0
         self.batch_size = cfg["BATCH_SIZE"]
-        self.states = []
-        self.actions = []
-        self.rewards = []
-        self.new_states = []
+        self.states = np.empty((self.max_buffer_size, state_size), dtype=np.float64)
+        self.actions = np.empty((self.max_buffer_size, action_size), dtype=np.uint8)
+        self.rewards = np.empty(self.max_buffer_size, dtype=np.integer)
+        self.new_states = np.empty((self.max_buffer_size, state_size), dtype=np.float64)
+        self.current_idx = 0
 
     def sample(self) -> tuple:
         """"
         Samples experiences from the buffer. The sampling size depends
         on the variable BATCH_SIZE in the config file.
         """
-        if self.cur_buffer_size <= self.batch_size:
-            sample_states = self.states
-            sample_actions = self.actions
-            sample_rewards = self.rewards
-            sample_new_states = self.new_states
+        if self.cur_buffer_size > self.batch_size:
+            indices = np.random.choice(self.cur_buffer_size, self.batch_size)
         else:
-            batch_ind = random.sample(range(self.cur_buffer_size), self.batch_size)
-            sample_states = itemgetter(*batch_ind)(self.states)
-            sample_actions = itemgetter(*batch_ind)(self.actions)
-            sample_rewards = itemgetter(*batch_ind)(self.rewards)
-            sample_new_states = itemgetter(*batch_ind)(self.new_states)
+            indices = range(self.cur_buffer_size)
+
+        sample_states = self.states[indices]
+        sample_actions = self.actions[indices]
+        sample_rewards = self.rewards[indices]
+        sample_new_states = self.new_states[indices]
 
         return sample_states, sample_actions, sample_rewards, sample_new_states
 
@@ -42,31 +42,15 @@ class ReplayBuffer(object):
         reached. A return of the env is an array of with size NUM_AGENTS X OBSERVATION_SPACE,
         so it has to be saved row by row.
         """
-        num_envs = len(state)
-        if isinstance(reward, np.ndarray):
-            if self.cur_buffer_size >= self.max_buffer_size:
-                del self.states[:num_envs]
-                del self.actions[:num_envs]
-                del self.rewards[:num_envs]
-                del self.new_states[:num_envs]
 
-            self.states.extend(state)
-            self.actions.extend(action)
-            self.rewards.extend(reward)
-            self.new_states.extend(new_state)
-            self.cur_buffer_size = len(self.states)
+        self.states[self.current_idx] = state
+        self.actions[self.current_idx] = action
+        self.rewards[self.current_idx] = reward
+        self.new_states[self.current_idx] = new_state
 
-        else:
-            if self.cur_buffer_size >= self.max_buffer_size:
-                del self.states[0]
-                del self.actions[0]
-                del self.rewards[0]
-                del self.new_states[0]
+        self.cur_buffer_size = max(self.cur_buffer_size, self.current_idx + 1)
+        self.current_idx = (self.current_idx + 1) % self.max_buffer_size
 
-            self.states.append(state.flatten())
-            self.actions.append(action.flatten())
-            self.rewards.append(reward)
-            self.new_states.append(new_state.flatten())
-            self.cur_buffer_size = len(self.states)
+    def current_size(self):
 
-
+        return self.cur_buffer_size
