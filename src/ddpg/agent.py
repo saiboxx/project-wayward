7a0bc99ef451bfeaa7c6330ff52ed2dbe94e13f9
Dervishi <erjona.dervishi@outlook.com>
2020-01-12 14:36:21 +0100

    Fix replay buffer constructor

--- src/ddpg/agent.py
+++ src/ddpg/agent.py
@@ -25,7 +25,7 @@ class DDPGAgent(object):
 
         self.actor = Actor(observation_space, action_space, self.device)
         self.critic = Critic(observation_space, action_space, self.device)
-        self.replay_buffer = ReplayBuffer()
+        self.replay_buffer = ReplayBuffer(observation_space, action_space)
         self.gamma = cfg["GAMMA"]
         self.tau = cfg["TAU"]
         self.output_path = os.path.join("models", cfg["EXECUTABLE"])
